475ebaee39fed3a14af9e86c49801ef27f4cb657
[Matthew Chang] <mrfchang03@gmail.com>
2020-12-02 10:02:15 -0800

    Added PracticeProject skeleton

---
+++ practiceproject.py
@@ -0,0 +1,13 @@
+# PracticeProject.py
+
+# Code to practice using Git
+
+def main():   # everything that exists is coded in the main function
+    pass
+
+
+
+if __name__ == "*__main__*":
+    main()
+
+
