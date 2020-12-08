f55d180d93bd13ab602fa5818d318bbf0d07e2ba
[Matthew Chang] <mrfchang03@gmail.com>
2020-12-08 10:37:57 -0800

    quizzypoop

--- Quizzypoo.py
+++ Quizzypoo.py
@@ -6,6 +6,7 @@ if question_One == "6" or question_One == "six":
         score += 1
 else:
     print("Try using a calculator")
+
 question_Two = input( "How many planets are there in the solar system?").lower()
 if question_Two== "8" or question_Two == "eight":
         print("You an astronaut?")
@@ -19,17 +20,19 @@ if question_Three == "Subaru" or question_Three == "Natsuki Subaru":
         score += 1
 else:
     print("Really? Watch it right now then!")
+
 question_Four = input( "Which does more damage in minecraft? The Axe or the Sword?").lower()
 if question_Four == "sword":
-        print("Correct")
+        print("Dream? U code?")
         score += 1
 else:
     print("Smh, I don't think you're playing enough!")
+
 question_Five = input( "What is the difference between a vector and scalar quantity?").lower()
 if question_Five == "direction and magnitude" or question_Five == "magnitude and direction":
-        print("Correct")
+        print("WOAHH, EINSTEINS ALIVE!")
         score += 1
 else:
-    print("Incorrect")
+    print("Aiya, pay attention in Clements!)
 
 print(f"Your final score is {score / 5 * 100} %")
