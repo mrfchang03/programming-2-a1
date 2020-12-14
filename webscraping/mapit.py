3be848f274c3132e8469f2a79e1f22e0513295a9
[Matthew Chang] <mrfchang03@gmail.com>
2020-12-14 10:09:40 -0800

    copy paste command

--- webscraping/mapit.py
+++ webscraping/mapit.py
@@ -1,9 +1,16 @@
 #mapit.py launches a map in a browser using and address from the command line
-import webbrowser, sys
+import webbrowser
+import sys
+import pyperclip
+
+prefix = "https://www.google.com/maps/place/"
+#If there are arguments, use args as address
+#If there are no arguments, grab from clipboard
 
 if len(sys.argv) > 1:
     address = " ".join(sys.argv[1:])
-    prefix = "https://www.google.com/maps/place/"
+    webbrowser.open(prefix + address)
 
-webbrowser.open(prefix + address)
-# Get the address from the clipboard
+else:
+    address = pyperclip.paste()
+    webbrowser.open(prefix + address)
